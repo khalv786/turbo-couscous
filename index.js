3b47bf8b3a9cefa49a2f677ee9d40472192ba0b2
Khalid Khan <up696798@myport.ac.uk>
2016-11-22 19:18:35 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,50 @@
+var express = require('express');
+
+var fs = require('fs');
+var path = require('path');
+var app = express();
+var http = require('http').Server(app);
+
+var numUsers = 0;
+
+app.use(express.static('public'));
+
+//app.configure(function () {
+//    app.set('port', process.env.PORT || 8000);
+//    app.use(express.static(path.join(__dirname, 'public')));
+//});
+//var server = http.createServer(app);
+//server.listen(app.get('port'), function () {
+//    console.log("Express server listening on port " + app.get('port'));
+//});
+
+//create a route
+app.get('/', function (req, res) {
+    res.sendfile('public/index.html');
+});
+
+var io = require('socket.io')(http);
+
+
+
+
+io.on('connection', function (socket) {
+
+
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+    });
+
+    socket.on('new polygon', function (msg) {
+        io.emit('new polygon', msg);
+    });
+
+});
+
+
+
+
+
+    http.listen(3000, function () {
+        console.log('listening on *:3000');
+    });
---
+++ public/main.js
@@ -0,0 +1,138 @@
+﻿var map;
+
+var source = new ol.source.Vector({ wrapX: false });
+
+source.set("name", "my_layer_name");
+
+
+
+window.onload = function init() {
+
+
+    //create a base vector layer to draw on
+    var vector = new ol.layer.Vector({
+        source: source,
+        style: new ol.style.Style({
+        fill: new ol.style.Fill({
+            color: 'rgba(255, 255, 255, 0.2)'
+        }),
+        stroke: new ol.style.Stroke({
+            color: '#ffcc33',
+            width: 2
+        }),
+        image: new ol.style.Circle({
+            radius: 7,
+            fill: new ol.style.Fill({
+                color: '#ffcc33'
+            })
+        })
+    })
+    });
+
+    var raster = new ol.layer.Tile({
+        source: new ol.source.OSM()
+    });
+
+
+
+
+    //create map
+ map = new ol.Map({
+    layers: [raster, vector],
+    target: 'map',
+    controls: ol.control.defaults({
+        attributionOptions: /** @type {olx.control.AttributionOptions} */ ({
+            collapsible: false
+        })
+    }),
+    view: new ol.View({
+        center: [0, 0],
+        zoom: 2
+    })
+    });
+
+
+}
+
+
+function addLayer() {
+    var layer = prompt("Please enter a layer name");
+
+    if (layer != null) {
+        var vector = new ol.layer.Vector({
+            title:layer,
+            source: source,
+            style: new ol.style.Style({
+                fill: new ol.style.Fill({
+                    color: 'rgba(255, 255, 255, 0.2)'
+                }),
+                stroke: new ol.style.Stroke({
+                    color: '#ffcc33',
+                    width: 2
+                }),
+                image: new ol.style.Circle({
+                    radius: 7,
+                    fill: new ol.style.Fill({
+                        color: '#ffcc33'
+                    })
+                })
+            })
+        });
+
+        map.addLayer(layer);
+        
+    }
+}
+
+    var draw; // global so we can remove it later
+    function drawPolygon(value) {
+        
+            var geometryFunction, maxPoints;
+            if (value === 'Square') {
+                value = 'Circle';
+                geometryFunction = ol.interaction.Draw.createRegularPolygon(4);
+            } else if (value === 'Box') {
+                value = 'LineString';
+                maxPoints = 2;
+                geometryFunction = function (coordinates, geometry) {
+                    if (!geometry) {
+                        geometry = new ol.geom.Polygon(null);
+                    }
+                    var start = coordinates[0];
+                    var end = coordinates[1];
+                    geometry.setCoordinates([
+                        [start, [start[0], end[1]], end, [end[0], start[1]], start]
+                    ]);
+                    return geometry;
+                };
+            }
+            var layers = map.getLayers();
+            draw = new ol.interaction.Draw({
+                source: source,
+                type: value,
+                geometryFunction: geometryFunction,
+                maxPoints: maxPoints
+            });
+            
+            map.addInteraction(draw);
+
+            draw.on('drawend', function (event) {
+
+                // Get the array of features
+                var features = source.getFeatures();
+
+                if (features.length != 0) {
+                    var last_element = features[features.length - 1];
+
+                    console.log(last_element.getGeometry().getCoordinates());
+                    socket.emit('new polygon', last_element.getGeometry().getCoordinates());
+                    socket.emit('chat message', last_element.getGeometry().getCoordinates());
+                }
+            });
+
+    }
+
+    
+
+
+    
